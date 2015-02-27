ba88b94461fe198b0751c9a876bf21c6d4968907
zaidedan <dan.zaide@gmail.com>
2015-02-27 14:09:06 -0500

    dskflsd

--- p/support/reveal.init.js
+++ p/support/reveal.init.js
@@ -31,12 +31,19 @@ Reveal.initialize({
 	{ src: '../reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
 	{ src: '../reveal.js/plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
 	{ src: '../reveal.js/plugin/math/math.js', async: true },
-  // { src: '//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js', async: true },
-  // { src: 'plugin/multiplex/client.js', async: true },
+  { src: '//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min.js', async: true },
+  { src: 'plugin/multiplex/client.js', async: true },
 
   // // and if you want speaker notes
   // { src: 'plugin/notes-server/client.js', async: true }
 
-	]
+	],
+  multiplex: {
+      // Example values. To generate your own, see the socket.io server instructions.
+      secret: 'null', // Obtained from the socket.io server. Gives this (the master) control of the presentation
+      id: 'ed399d44352e1d88', // Obtained from socket.io server
+      url: 'revealjs.jit.su:80' // Location of socket.io server
+  },
+
 
 });
