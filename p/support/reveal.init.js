4c63900ab13906968dd275dceea2aa80e6a79531
Daniel Zaide <dan.zaide@gmail.com>
2017-05-01 21:34:09 -0700

    changed mathjax

--- p/support/reveal.init.js
+++ p/support/reveal.init.js
@@ -10,7 +10,7 @@ Reveal.initialize({
 	transition: Reveal.getQueryHash().transition || 'linear', // default/cube/page/concave/zoom/linear/fade/none
 	slideNumber: false,
 	math: {
-		mathjax: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js',
+		mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js',
 		config: 'TeX-AMS_HTML-full'
 	},
   width: 1024,
